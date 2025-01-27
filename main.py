0ef4f088e50c8f7b3cabf102e4b98b769927fd46
Varvara <v20004565@gmail.com>
2025-01-27 16:52:42 +0400

    1/2

---
+++ main.py
@@ -0,0 +1,42 @@
+MorseCode = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.-', '-': '-....-', '(': '-.--.', ')': '-.--.-'
+}
+
+
+def encode_to_morse(text):
+
+    morse_code = ""
+    text = text.upper()
+    for char in text:
+        if char != ' ':
+            if char in MorseCode:
+                morse_code += MorseCode[char] + " "
+            else:
+                return
+        else:
+            morse_code += "  "
+    return morse_code.strip()
+
+
+
+def main():
+    while True:
+        action = input().lower()
+        if action == 'q':
+            break
+        elif action == 'e':
+            text = input()
+            encoded_text = encode_to_morse(text)
+            print(encoded_text)
+        elif action == 'd':
+            morse_code = input()
+            decoded_text = decode_from_morse(morse_code)
+            print(decoded_text)
+
